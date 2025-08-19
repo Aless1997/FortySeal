06468665dc8d5ae77c0f032d8723e35ab5118953
Alessandro <90648026+Aless1997@users.noreply.github.com>
2025-08-19 20:24:26 +0200

    Update settings.py
    fghd

--- Cripto/settings.py
+++ Cripto/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'block-lxlw.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'block-lxlw.onrender.com', 'fortyseal-1.onrender.com']
 
 
 
