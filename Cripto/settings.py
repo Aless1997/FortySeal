4873e64bb230d24f46b957919c9fce73e0ee4c45
Aless1997 <alessandro.sinigaglia1997@gmail.com>
2025-09-14 11:23:56 +0200

    Delete Celery 2

--- Cripto/settings.py
+++ Cripto/settings.py
@@ -180,11 +180,3 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
-
-# Mantieni solo:
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
