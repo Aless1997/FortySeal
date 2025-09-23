5f8edf48e59a7ada2e9f5783d4edc01f85459e16
Aless1997 <alessandro.sinigaglia1997@gmail.com>
2025-09-23 14:32:11 +0200

    settings.py

--- Cripto/settings.py
+++ Cripto/settings.py
@@ -144,6 +144,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Cripto1', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
